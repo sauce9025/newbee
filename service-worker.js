19f89ad9314dfcd1e8c8000f2d657602e99c5f64
sauce9025 <jjjeongsh9025@gmail.com>
2025-06-25 16:40:30 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'escape-game-v1';
+const urlsToCache = [
+    '/newbee/',
+    '/newbee/index.html',
+    '/newbee/manifest.json',
+    'img src="https://i.ibb.co/trrZh9y/maskable-icon-x128.png',
+    'https://i.ibb.co/7dJJdggX/maskable-icon-x512.png',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return new Response('오프라인 상태입니다.');
+            })
+    );
+});
